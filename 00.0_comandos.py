2f12830db6dccb28ab49939c1925ff0783e9e212
j-porcel <jporcel@proton.me>
2024-07-16 19:22:43 +0200

    Add files via upload

---
+++ 00.0_comandos.py
@@ -0,0 +1,11 @@
+#Ejecutar por terminal
+
+#Crear maquina virtual(mv)
+#python -m venv nombre_de_la_mv
+
+#Dejar en un archivo todos las librerías instaladas en el ambiente (mv)
+#pip install freeze-requeriments
+#pip freeze > requerimientos.txt
+
+
+
